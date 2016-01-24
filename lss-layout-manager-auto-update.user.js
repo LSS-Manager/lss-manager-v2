d3f1a02b15a9736742acd2dc00def36abcc130cd
lost <weller.andrethomas@web.de>
2016-01-24 02:38:51 +0100

    Update lss-layout-manager-auto-update.user.js

--- lss-layout-manager-auto-update.user.js
+++ lss-layout-manager-auto-update.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS Layout Manger Auto Update
 // @namespace    http://www.lss-manager.de
-// @version      2.2
+// @version      2.3
 // @description  Mit der Tastatur Alarmieren oder sonstiges
 // @author       lost & northdegree
 // @include      http://www.leitstellenspiel.de/
@@ -12,7 +12,5 @@
 // @grant        none
 // @run          document-start
 // ==/UserScript==
-/* jshint -W097 */
-'use strict';
 
 $('head').append('<script src="http://lss.lost.design/lss-layout-manager-2.dev.js" type="text/javascript"></script>');
