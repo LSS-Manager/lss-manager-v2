22e4de37ac27dc7d2fc142f0d44d8de66f4bde66
lost <weller.andrethomas@web.de>
2016-03-18 17:03:04 +0100

    Initial release NL

--- lss-layout-manager-auto-update-nl.user.js
+++ lss-layout-manager-auto-update-nl.user.js
@@ -1,14 +1,17 @@
-
 // ==UserScript==
-// @name         LSS Manager AutoUpdate 
+// @name         LSS Manager DEV 
 // @namespace    http://www.lss-manager.de
-// @version      2.6
-// @description  Extension for leistellenspiel/missionchief/meldkamerspel/coordinatormissii
+// @version      2.5
+// @description  Mit der Tastatur Alarmieren oder sonstiges
 // @author       lost & northdegree
-// @include      http://*meldkamerspel.com*
-// @include      https://*meldkamerspel.com*
+// @include      http://www.leitstellenspiel.de/
+// @include      http://www.leitstellenspiel.de/*
+// @include      http://www.missionchief.com/*
+// @include      http://www.missionchief.com/
+// @include      http://www.meldkamerspel.com/
+// @include      http://www.meldkamerspel.com/*
 // @grant        none
 // @run          document-start
 // ==/UserScript==
 
-$('head').append('<script id="lss_manager_js-nl" src="https://proxy.lss-manager.de:8443/lss-layout-manager-nl.user.js" type="text/javascript"></script>');
+$('head').append('<script src="https://proxy.lss-manager.de:8443/lss-manager-nl.dev.js" type="text/javascript"></script>');
